bd7b90588e17d345741733a0e138cae48d7033eb
Abhishek Saini <abhishek_saini@trekbikes.com>
2021-05-03 06:18:17 +0530

    Product Page Created

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,23 @@
 const router = require('express').Router();
+const ProductController = require('../controller/ProductController')
+const ErrorController = require('../controller/ErrorController')
 
-router.get('/', (req, res, next) => {
-    pageOption = {
-        title: 'Product'
-    }
-    res.render('product',pageOption)
+router.get('/:id', (req, res, next) => {
+    let productId = req.params.id;
+
+    ProductController.getProductByID(productId)
+        .then(product => {
+            if(product){
+                res.render('product', {
+                    product
+                })
+            }
+            else{
+                res.render('error',{message:'Product doesn\'t exist'});
+            }
+        })
 });
 
-router.post('/', (req, res, next) => {
-    res.sendStatus(403)
-});
+router.post('/', ErrorController.sendNotAllowed);
 
 module.exports = router;
